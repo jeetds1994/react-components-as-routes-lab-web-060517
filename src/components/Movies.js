3db524f342fe9ac40f4d97ec08a18bb5f2e22857
Diljeet Singh <jeetds1994@gmail.com>
2017-08-14 11:33:59 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,24 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  debugger
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie) => {
+        return(
+        <div>
+          <h1>Title: {movie.title}</h1>
+          <p>Time: {movie.time}</p>
+          <li>
+            {movie.genres.map((genre) => {
+              return (
+                <ul>{genre}</ul>
+              )
+            })}
+          </li>
+        </div>
+      )})}
     </div>
   );
 };
